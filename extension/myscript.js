66a06ff9a173efc7a94862aa8a139a5df22b801a
Vikram Santhalia <vikram.san@vikram.santhalia>
2016-07-22 03:46:54 +0530

    ajax call with json data

--- extension/myscript.js
+++ extension/myscript.js
@@ -4,14 +4,28 @@ function showSearchButton() {
   var searchDiv = $("<div></div>").attr('id',SEARCH_DIV).appendTo('#fk-mainbody-id .fk-content');
   $.get(chrome.extension.getURL('/templates/search_div.html'), function(data) {
     $($.parseHTML(data)).appendTo('#'+SEARCH_DIV);
-    setupForm();
-
+    $("#submit").on('click', function(event){
+            var data = {
+                "query" : {
+                   "match" : {"name" : $("#search_input").val()}
+                   }
+                };
+            $.ajax({
+                url: 'http://172.20.165.104:3000/data',
+                type : "POST",
+                dataType : 'json',
+                data : JSON.stringify(data),
+                success : renderSearchResults,
+                error: function(xhr, resp, text) {
+                    console.log(xhr, resp, text);
+                }
+            })
+            event.preventDefault();
+        });
   });
 }
 
 showSearchButton();
-
-
 
 function getProductTitle() {
   var productTitle = $('.product-details .title-wrap h1')[0];
@@ -19,12 +33,7 @@ function getProductTitle() {
   return productTitle.innerHTML;
 }
 
-function setupForm() {
-  $("#review_search").ajaxForm({url: 'http://localhost:3000/data', type: 'post', success: renderSearchResults})
-}
-
 function renderSearchResults(res, status, xhr, form) {
-  console.log(res);
   $("#" + SEARCH_DIV).append(document.createTextNode( JSON.stringify(res) ));
 
 }
