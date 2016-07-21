70d9dcd4ddd1eb8ce2d9a64836bb14840d1aa326
Ashish Chaturvedi <ashish.chaturvedi@flipkart.com>
2016-07-22 02:34:41 +0530

    WIP: hitting mock search

--- extension/myscript.js
+++ extension/myscript.js
@@ -1,14 +1,25 @@
-var productTitle = $('.product-details .title-wrap h1')[0];
-console.log(productTitle.innerHTML);
-
 function showSearchButton() {
   var searchDiv = $("<div></div>").attr('id','searchDiv').appendTo('#fk-mainbody-id .fk-content');
-  // searchDiv.load("search_div.html");
   $.get(chrome.extension.getURL('/templates/search_div.html'), function(data) {
     $($.parseHTML(data)).appendTo('#searchDiv');
-    // Or if you're using jQuery 1.8+:
-    // $($.parseHTML(data)).appendTo('body');
+    setupForm();
+
   });
 }
 
 showSearchButton();
+
+function getProductTitle() {
+  var productTitle = $('.product-details .title-wrap h1')[0];
+  console.log(productTitle.innerHTML);
+  return productTitle.innerHTML;
+}
+
+function setupForm() {
+  $("#review_search").ajaxForm({url: 'http://localhost:3000/data', type: 'post', success: renderSearchResults})
+}
+
+function renderSearchResults(res, status, xhr, form) {
+  console.log(res);
+
+}
