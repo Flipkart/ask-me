8641a8f623fe77009c97c58b31075e82a9e8341a
Ashish Chaturvedi <ashish.chaturvedi@flipkart.com>
2016-07-22 04:27:41 +0530

    Fixing search

--- extension/myscript.js
+++ extension/myscript.js
@@ -1,31 +1,32 @@
 var SEARCH_DIV = 'searchDiv';
+var SEARCH_RESPONSE_CLASS = 'review_search_response';
+
 
 function showSearchButton() {
   var searchDiv = $("<div></div>").attr('id',SEARCH_DIV).appendTo('#fk-mainbody-id .fk-content');
   $.get(chrome.extension.getURL('/templates/search_div.html'), function(data) {
     $($.parseHTML(data)).appendTo('#'+SEARCH_DIV);
-    $("#submit").on('click', function(event){
-            var data = {
-                "query" : {
-                   "match" : {"name" : $("#search_input").val()}
-                   }
-                };
-            $.ajax({
-                url: 'http://172.20.165.104:3000/data',
-                type : "POST",
-                dataType : 'json',
-                data : JSON.stringify(data),
-                success : renderSearchResults,
-                error: function(xhr, resp, text) {
-                    console.log(xhr, resp, text);
-                }
-            })
-            event.preventDefault();
-        });
+    searchDiv.find("#submit").on('click', searchSubmitHandler);
   });
 }
 
-showSearchButton();
+function searchSubmitHandler(event){
+  var data = {
+    "product_name" : getProductTitle(),
+    "query" : $("#"+ SEARCH_DIV + " #search_input").val()
+  };
+  $.ajax({
+    url: 'http://172.20.165.104:3000/data',
+    type : "POST",
+    dataType : 'json',
+    data : JSON.stringify(data),
+    success : renderSearchResults,
+    error: function(xhr, resp, text) {
+      console.log(xhr, resp, text);
+    }
+  });
+  event.preventDefault();
+}
 
 function getProductTitle() {
   var productTitle = $('.product-details .title-wrap h1')[0];
@@ -34,6 +35,7 @@ function getProductTitle() {
 }
 
 function renderSearchResults(res, status, xhr, form) {
-  $("#" + SEARCH_DIV).append(document.createTextNode( JSON.stringify(res) ));
-
+  $("#" + SEARCH_DIV + " ." + SEARCH_RESPONSE_CLASS).html( JSON.stringify(res) );
 }
+
+showSearchButton();
